99cbd2f912d76ba2d9e5cee3758808f541945c96
Harsimran12003 <harsimran12003@gmail.com>
2026-02-14 01:28:47 +0530

    made backend7

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,24 @@ dotenv.config();
 connectDB();
 
 const app = express();
+const allowedOrigins = [
+  "https://tech-agro.vercel.app",
+  "https://www.techagro.co.in",
+  "https://techagro.co.in"
+];
 
 
 app.use(
   cors({
-    origin: "https://tech-agro.vercel.app", 
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
